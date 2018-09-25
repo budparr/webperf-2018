12d86750be08c73b638e9526b1e10e5ae83d038e
budparr <budparr@gmail.com>
2018-09-24 22:55:53 -0400

    test w/out turbolinks

--- assets/index.js
+++ assets/index.js
@@ -3,7 +3,7 @@ require("typeface-open-sans")
 import styles from "./css/styles.css"
 import "./js/algolia/instantsearch.js"
 import "./js/lazysizes.js"
-import "./js/turbolinks.js"
+// import "./js/turbolinks.js"
 import "./js/nojs.js"
 
 
